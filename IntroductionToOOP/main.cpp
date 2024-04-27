71c73b64671bcc779eb38f1cf9ae5a917bc7f9a1
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-04-28 00:07:39 +1000

    ClassMethodDistance_FunctionDistance

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 
@@ -6,7 +7,21 @@ class Point  // Создаем структуру 'Point', которая будет описывать точки на плос
 {
 	double x;
 	double y;
+
 public:
+	//Point() { x = 0; y = 0; }; // default constructor 
+
+	//Point(double X, double Y)   // constructor 2
+	//{
+	//	x = X;
+	//	y = Y;
+	//}
+
+	//Point(double X, double Y) :x(X), y(Y) {}; //constructor with member initialization list functional form
+
+	Point(double X, double Y) :x{ X }, y{ Y } {}; // uniform initializer syntax
+
+
 	double get_x() const
 	{
 		return x;
@@ -23,7 +38,25 @@ public:
 	{
 		this -> y = y;
 	}
+	/*template <typename T>  T Power(T num, int n)
+	{
+		double pow = 1;
+		if (n < 0)
+		{
+			num = 1 / num;
+			n = -n;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			pow *= num;
+		}
+		return pow;
+	}*/
+	
+	double distance();
 };
+
+double distance(Point, Point);
 
 //#define STRUCT_POINT
 
@@ -47,10 +80,25 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;      //  одно и то же  
 #endif // STRUCT_POINT
 
-	Point A;
-	A.set_x(2);
+	Point A{3, 4};    //  uniform init  defoult constructor called
+	//Point B(8,6);  // functional form initialization defoult constructor NOT called
+	Point B{8, 9}; 
+	/*A.set_x(2);
 	A.set_y(3);
-	cout << A.get_x() << "\t" << A.get_y() << endl;
+	B.set_x(5);
+	B.set_y(8);*/
 
+	std::cout << A.get_x() << "\t" << A.get_y() << endl;
+	std::cout << B.get_x() << "\t" << B.get_y() << endl;
+	std::cout << "The distance to point B is: " << B.distance() << endl;
+	std::cout << "The distance between points X and Y is: " << distance(A,B) << endl;
+}
 
+double Point::distance() { return pow(pow(x, 2) + pow(y, 2), 0.5); }
+
+double distance(Point x, Point y)
+{
+	double deltX = y.get_x() - x.get_x();
+	double deltY = y.get_y() - x.get_y();
+	return  pow(pow(deltX, 2) + pow(deltY, 2), 0.5);
 }
